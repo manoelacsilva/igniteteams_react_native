e049428f3b35a5b259e5aa3eced2c826e9c3d75e
manoelacsilva <manoelacs62@gmail.com>
2025-03-14 19:09:06 -0300

    feat: criação de componentes

--- App.tsx
+++ App.tsx
@@ -1,7 +1,24 @@
-import { Groups } from './src/screens/Groups'
+import { StatusBar } from 'react-native'
+import { ThemeProvider } from 'styled-components'
+import { useFonts, Roboto_400Regular, Roboto_700Bold  } from '@expo-google-fonts/roboto'
+
+import { Loading } from '@components/Loading'
+
+import theme from './src/theme'
+
+import { Groups } from '@screens/Groups'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
+
   return (
-    <Groups />
+    <ThemeProvider theme={theme}>
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <Groups /> : <Loading />}
+    </ThemeProvider>
   )
 }
