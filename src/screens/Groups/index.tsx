2d5d002a40ad9e4e3560f9adc7eb996cb290001f
manoelacsilva <manoelacs62@gmail.com>
2025-03-17 17:18:36 -0300

    feat: criando interface newgroup

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,17 @@
+import { useState } from 'react'
+
 import { Header } from '@components/Header'
 import { Highlight } from '@components/Highlight'
-import { GroupCard } from '@components/GroupCard'
+import { ListEmpty } from '@components/ListEmpty'
+import { Button } from '@components/Button'
 
 import { Container } from './styles'
+import { FlatList } from 'react-native'
+import { GroupCard } from '@components/GroupCard'
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>([])
+
   return (
     <Container>
       <Header />
@@ -12,7 +19,24 @@ export function Groups() {
         title="Turmas"
         subtitle='jogue com a sua turma'
       />
-      <GroupCard title="Galera do Ignite" />
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard 
+            title={item} 
+          />
+        )}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty 
+            message='Que tal cadastrar a primeira turma?' 
+          />
+        )}
+      />
+      <Button 
+        title='Criar nova turma'
+      />
     </Container>
   )
 }
