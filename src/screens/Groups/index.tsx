04a67ce5ecba679e2b45fdeb3500634db5c94bbd
manoelacsilva <manoelacs62@gmail.com>
2025-03-19 17:04:48 -0300

    feat: implementando o voltar

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,16 +1,23 @@
 import { useState } from 'react'
+import { FlatList } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
 
 import { Header } from '@components/Header'
 import { Highlight } from '@components/Highlight'
 import { ListEmpty } from '@components/ListEmpty'
 import { Button } from '@components/Button'
+import { GroupCard } from '@components/GroupCard'
 
 import { Container } from './styles'
-import { FlatList } from 'react-native'
-import { GroupCard } from '@components/GroupCard'
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([])
+
+  const navigation = useNavigation()
+
+  function handleNewGroup() {
+    navigation.navigate('new')
+  }
 
   return (
     <Container>
@@ -37,6 +44,7 @@ export function Groups() {
       />
       <Button 
         title='Criar nova turma'
+        onPress={handleNewGroup}
       />
     </Container>
   )
