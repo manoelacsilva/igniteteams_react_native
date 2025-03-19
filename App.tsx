bc20e8d0b4d9803512fcc67c6cf191cadfcc788e
manoelacsilva <manoelacs62@gmail.com>
2025-03-19 12:54:06 -0300

    feat: finalizando o css-in-js da aplicação

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from 'react-native'
-import { ThemeProvider } from 'styled-components'
+import { ThemeProvider } from 'styled-components/native'
 import { useFonts, Roboto_400Regular, Roboto_700Bold  } from '@expo-google-fonts/roboto'
 
 import { Loading } from '@components/Loading'
@@ -7,6 +7,7 @@ import { Loading } from '@components/Loading'
 import theme from './src/theme'
 
 import { Players } from '@screens/Players';
+// import { Groups } from '@screens/Groups';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
