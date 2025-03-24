709dab382bdc0de0c7588f025bd866ad0790154f
manoelacsilva <manoelacs62@gmail.com>
2025-03-24 16:51:41 -0300

    feat

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,6 +1,12 @@
 import { useState } from 'react'
-import { FlatList } from 'react-native'
+import { FlatList, Alert } from 'react-native'
 import { useRoute } from '@react-navigation/native'
+
+import { AppError } from '@utils/AppError'
+
+import { PlayerStorageDTO } from '@storage/player/PlayerStorageDTO'
+import { playerAddByGroup } from '@storage/player/playerAddByGroup'
+import { playersGetByGroupAndTeam } from '@storage/player/playersGetByGroupAndTeam'
 
 import { Header } from "@components/Header"
 import { Highlight } from "@components/Highlight"
@@ -18,11 +24,45 @@ type RouteParams = {
 }
 
 export function Players() {
+  const [newPlayerName, setNewPlayerName] = useState('')
   const [team, setTeam] = useState('Time A')
-  const [players, setPlayers] = useState([])
+  const [players, setPlayers] = useState<PlayerStorageDTO[]>([])
 
   const route = useRoute()
+
   const { group } = route.params as RouteParams
+
+  async function handleAddPlayer() {
+    if(newPlayerName.trim().length === 0) {
+      return Alert.alert('Nova pessoa', 'Informe o nome da pessoa para adicionar.')
+    }
+
+    const newPlayer = {
+      name: newPlayerName,
+      team,
+    }
+
+    try {
+      await playerAddByGroup(newPlayer, group)
+    } catch (error) {
+      if(error instanceof AppError){
+        Alert.alert('Nova pessoa', error.message)
+      } else {
+        console.log(error)
+        Alert.alert('Nova pessoa', 'Não foi possível adicionar.')
+      }
+    }
+  }
+
+  async function fetchPlayersByTeam() {
+    try {
+      const playersByTeam = await playersGetByGroupAndTeam(group, team)
+      setPlayers(playersByTeam)
+    } catch (error) {
+      console.log(error)
+      Alert.alert('Pessoas', 'Não foi possível carregar as pessoas do time selecionado.')
+    }
+  }
   
   return (
     <Container>
@@ -35,9 +75,11 @@ export function Players() {
         <Input 
           placeholder="Nome da pessoa"
           autoCorrect={false}
+          onChangeText={setNewPlayerName}
         />
         <ButtonIcon 
-          icon="add" 
+          icon="add"
+          onPress={handleAddPlayer}
         />
       </Form>
       <HeaderList>
